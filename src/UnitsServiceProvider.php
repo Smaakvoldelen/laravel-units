99ab0eaafa03937639b88b0c922edc8dadd38564
Michael Beers <michael@michaelbeers.nl>
2024-04-26 11:14:53 +0200

    Clean up the package

--- src/UnitsServiceProvider.php
+++ src/UnitsServiceProvider.php
@@ -4,22 +4,16 @@ namespace Smaakvoldelen\Units;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Smaakvoldelen\Units\Commands\UnitsCommand;
 
 class UnitsServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Configure the package.
+     */
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-units')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-units_table')
-            ->hasCommand(UnitsCommand::class);
+            ->hasConfigFile();
     }
 }

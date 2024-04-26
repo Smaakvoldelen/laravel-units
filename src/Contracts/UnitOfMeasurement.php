9faa4cff576b63089d2eafb40004ea5ecd34b94d
Michael Beers <michael@michaelbeers.nl>
2024-04-26 11:15:32 +0200

    Add a contract for a unit of measurement

---
+++ src/Contracts/UnitOfMeasurement.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Smaakvoldelen\Units\Contracts;
+
+use Illuminate\Support\Collection;
+
+interface UnitOfMeasurement
+{
+    /**
+     * Get the unit by the given alias name.
+     */
+    public static function alias(string $aliasName): self;
+
+    /**
+     * Get all the unit values.
+     */
+    public static function getAllValues(): Collection;
+
+    /**
+     * Get all the unit values translated.
+     */
+    public static function getAllValuesTranslated(): Collection;
+
+    /**
+     * Convert the given value to the given unit value.
+     */
+    public function convert(float $value, UnitOfMeasurement $unit): float;
+
+    /**
+     * Convert the given value from given the base units value.
+     */
+    public function convertFromBaseValue(float $value): float;
+
+    /**
+     * Convert the given value to the base units value.
+     */
+    public function convertToBaseValue(float $value): float;
+
+    /**
+     * Get the translated value of the unit.
+     */
+    public function getValueTranslated(): string;
+}
